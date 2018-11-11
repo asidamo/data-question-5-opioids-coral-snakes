176afe7a9998d59091e9f38c9c64e81e01e6c071
David Haines <davidwhainesiii@gmail.com>
2018-11-10 21:39:58 -0600

    Sliced dataframe and initial analysis

--- dq5_opiates.R
+++ dq5_opiates.R
@@ -5,17 +5,45 @@
 library(tidyverse)
 library(magrittr)
 library(ggplot2)
+library(beeswarm)
+library(dplyr)
 
-opioids <- read.csv('data/opioids.csv')
-overdoses <- read.csv('data/overdoses.csv')
-prescribers <- read.csv('data/prescriber-info.csv')
+opioids_raw <- read.csv('data/opioids.csv')
+overdoses_raw <- read.csv('data/overdoses.csv')
+prescribers_raw <- read.csv('data/prescriber-info.csv')
 
-rx_per_prescriber <- gather(prescribers, rx, countrx, ABILIFY:ZOLPIDEM.TARTRATE)
+# Using gather to associate counts of prescriptions of individual drugs with providers.
+rx_per_npi <- gather(prescribers, rx, countrx, ABILIFY:ZOLPIDEM.TARTRATE)
 
-generics <- unique(opioids$Generic.Name)
+# 10 drugs are identified as opioids from opioids.csv. Non-matching labels require manual 
+# construction of a matching vector
+matches <- c("ACETAMINOPHEN.CODEINE", "FENTANYL", "HYDROMORPHONE.HCL", "METHADONE.HCL",
+             "MORPHINE.SULFATE", "MORPHINE.SULFATE.ER", "OXYCODONE.HCL", "OXYCODONE.ACETAMINOPHEN",
+             "OXYCONTIN", "TRAMADOL.HCL")
 
+matching_records <- rx_per_npi$rx %in% matches
 
-rx_per_prescriber[rx_per_prescriber$rx %in% generics]
+# Missing opioids from prescriber data:
+# Buprenorphine, Buprenorphine HCl, BUTALBIT/ACETAMIN/CAFF/CODEINE, BUTORPHANOL TARTRATE
+# CODEINE SULFATE, CODEINE/BUTALBITAL/ASA/CAFFEIN, CODEINE/CARISOPRODOL/ASPIRIN, DHCODEINE BT/ACETAMINOPHN/CAFF
+# DIHYDROCODEINE/ASPIRIN/CAFFEIN, HYDROCODONE BITARTRATE, HYDROCODONE/ACETAMINOPHEN, HYDROCODONE/IBUPROFEN
+# HYDROMORPHONE HCL/PF, IBUPROFEN/OXYCODONE HCL, LEVORPHANOL TARTRATE, MEPERIDINE HCL, MEPERIDINE HCL/PF, NALBUPHINE HCL,
+# OPIUM TINCTURE, OPIUM/BELLADONNA ALKALOIDS, OXYCODONE HCL/ASPIRIN, PENTAZOCINE HCL/ACETAMINOPHEN, PENTAZOCINE HCL/NALOXONE HCL,
+# PENTAZOCINE LACTATE, TAPENTADOL HCL, TRAMADOL HCL/ACETAMINOPHEN
 
+sum(prescribers$Opioid.Prescriber)
 
+# 14,688 prescribers have value of Opioid Prescriber = 1, assumed to be True
 
+# Slicing gathered dataset for only opioid drugs and Opioid prescribers. 
+opioid_per_npi <- rx_per_npi[matching_records == TRUE,]
+
+opioid_bynpi_hirx <- opioid_per_npi[opioid_per_npi$Opioid.Prescriber == 1,]
+non_prescribers <- opioid_per_npi[opioid_per_npi$Opioid.Prescriber == 0,]
+
+# Need to add mean, max, summaries/bar plots
+opioid_bynpi_hirx %>% 
+  group_by(rx) %>% 
+  summarise(sd(countrx))
+
+drugs <- unique(rx_per_npi$rx)
