04aa23844c3d78a4a8febd2f46422caf8bd60423
David Haines <davidwhainesiii@gmail.com>
2018-11-11 11:24:12 -0600

    additional subsetting of dataframes

--- dq5_opiates.R
+++ dq5_opiates.R
@@ -1,6 +1,15 @@
-# Opioids data - remove duplicates from Generic.Name - only field needed
-# prescribers data - split drug data from prescriber data in order
-#                    to rotate drug data
+# Things to do:
+# ~ summarize opioid prescribers by state
+# ~ States in 95th percentile for prescriptions and deaths
+# ~ Concentration of prescribers in states
+# ~ Seems likely that a prescription is the result of a diagnosis. 
+#   ~ Do we have data on diagnosis billing codes used in association with rxs?
+# ~ Compare opioid data frame prescription rates to overall rates for all drugs
+# ~ Compare overall rx rates by state.  Compare overall rx rates of opioid v non-opioid prescribers 
+# ~ Compare proportions of opioid prescriptions between populations/states
+#
+#
+#
 
 library(tidyverse)
 library(magrittr)
@@ -13,7 +22,9 @@ overdoses_raw <- read.csv('data/overdoses.csv')
 prescribers_raw <- read.csv('data/prescriber-info.csv')
 
 # Using gather to associate counts of prescriptions of individual drugs with providers.
-rx_per_npi <- gather(prescribers, rx, countrx, ABILIFY:ZOLPIDEM.TARTRATE)
+all_rx_per_npi <- gather(prescribers_raw, rx, countrx, ABILIFY:ZOLPIDEM.TARTRATE)
+all_rx_op <- all_rx_per_npi[all_rx_per_npi$Opioid.Prescriber == 1,]
+all_rx_non <- all_rx_per_npi[all_rx_per_npi$Opioid.Prescriber == 0,]
 
 # 10 drugs are identified as opioids from opioids.csv. Non-matching labels require manual 
 # construction of a matching vector
@@ -21,17 +32,18 @@ matches <- c("ACETAMINOPHEN.CODEINE", "FENTANYL", "HYDROMORPHONE.HCL", "METHADON
              "MORPHINE.SULFATE", "MORPHINE.SULFATE.ER", "OXYCODONE.HCL", "OXYCODONE.ACETAMINOPHEN",
              "OXYCONTIN", "TRAMADOL.HCL")
 
-matching_records <- rx_per_npi$rx %in% matches
+matching_records <- all_rx_per_npi$rx %in% matches
 
 # Missing opioids from prescriber data:
-# Buprenorphine, Buprenorphine HCl, BUTALBIT/ACETAMIN/CAFF/CODEINE, BUTORPHANOL TARTRATE
-# CODEINE SULFATE, CODEINE/BUTALBITAL/ASA/CAFFEIN, CODEINE/CARISOPRODOL/ASPIRIN, DHCODEINE BT/ACETAMINOPHN/CAFF
-# DIHYDROCODEINE/ASPIRIN/CAFFEIN, HYDROCODONE BITARTRATE, HYDROCODONE/ACETAMINOPHEN, HYDROCODONE/IBUPROFEN
-# HYDROMORPHONE HCL/PF, IBUPROFEN/OXYCODONE HCL, LEVORPHANOL TARTRATE, MEPERIDINE HCL, MEPERIDINE HCL/PF, NALBUPHINE HCL,
-# OPIUM TINCTURE, OPIUM/BELLADONNA ALKALOIDS, OXYCODONE HCL/ASPIRIN, PENTAZOCINE HCL/ACETAMINOPHEN, PENTAZOCINE HCL/NALOXONE HCL,
-# PENTAZOCINE LACTATE, TAPENTADOL HCL, TRAMADOL HCL/ACETAMINOPHEN
+# Buprenorphine, Buprenorphine HCl, BUTALBIT/ACETAMIN/CAFF/CODEINE, BUTORPHANOL TARTRATE,
+# CODEINE SULFATE, CODEINE/BUTALBITAL/ASA/CAFFEIN, CODEINE/CARISOPRODOL/ASPIRIN, 
+# DHCODEINE BT/ACETAMINOPHN/CAFF, DIHYDROCODEINE/ASPIRIN/CAFFEIN, HYDROCODONE BITARTRATE, 
+# HYDROCODONE/ACETAMINOPHEN, HYDROCODONE/IBUPROFEN, HYDROMORPHONE HCL/PF, IBUPROFEN/OXYCODONE HCL,
+# LEVORPHANOL TARTRATE, MEPERIDINE HCL, MEPERIDINE HCL/PF, NALBUPHINE HCL,
+# OPIUM TINCTURE, OPIUM/BELLADONNA ALKALOIDS, OXYCODONE HCL/ASPIRIN, PENTAZOCINE HCL/ACETAMINOPHEN, 
+# PENTAZOCINE HCL/NALOXONE HCL, PENTAZOCINE LACTATE, TAPENTADOL HCL, TRAMADOL HCL/ACETAMINOPHEN
 
-sum(prescribers$Opioid.Prescriber)
+sum(prescribers_raw$Opioid.Prescriber)
 
 # 14,688 prescribers have value of Opioid Prescriber = 1, assumed to be True
 
@@ -46,4 +58,17 @@ opioid_bynpi_hirx %>%
   group_by(rx) %>% 
   summarise(sd(countrx))
 
-drugs <- unique(rx_per_npi$rx)
+total_op_rx <- opioid_bynpi_hirx %>% 
+  group_by(State) %>% 
+  summarise(sum(countrx))
+mean(total_op_rx$`sum(countrx)`)
+non_prescribers %>% 
+  group_by(State) %>% 
+  summarise(sum(countrx))
+
+
+
+
+
+
+
