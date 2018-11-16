c6f45af990d025b7be892715c2f50906d2047591
David Haines <davidwhainesiii@gmail.com>
2018-11-15 20:01:51 -0600

    Thursday work, updating datasets for further analysis

--- dq5_opiates.R
+++ dq5_opiates.R
@@ -21,25 +21,17 @@ library(ggplot2)
 library(beeswarm)
 library(dplyr)
 
-opioids_raw <- read.csv('data/opioids.csv')
-overdoses_raw <- read.csv('data/overdoses.csv')
-prescribers_raw <- read.csv('data/prescriber-info.csv')
-prescribers_16_raw <- read.csv('prescriber-info_CY16.csv')
+opioids_raw <- read_csv('data/opioids.csv')
+overdoses_raw <- read_csv('data/overdoses.csv')
+prescribers_raw <- read_csv('data/prescriber-info.csv')
+prescribers_16_raw <- read_csv('prescriber-info_CY16.csv')
 
-opioids_raw <- as.data.frame(opioids_raw)
-overdoses_raw <- as.data.frame(overdoses_raw)
-prescribers_raw <- as.data.frame(prescribers_raw)
-prescribers_16_raw <- as.data.frame(prescribers_16_raw)
-unique(prescribers_raw$State)
-unique(overdoses_raw$State)
+#non-states identified, dropping from raw data
+
 nonstates <- c("PR", "ZZ", "AA", "AE", "GU","VI","DC")
 
 nonstatematches <- prescribers_raw$State %in% nonstates
-prescribers_raw <- filter(prescribers_raw, prescribers_raw$State != "PR", prescribers_raw$State != "ZZ",
-                          prescribers_raw$State != "AA", prescribers_raw$State != "AA", prescribers_raw$State != "AE",
-                          prescribers_raw$State != "GU", prescribers_raw$State != "VI", prescribers_raw$State != "DC")
-npis <- unique(prescribers_raw$NPI)
-unique(count(npis))
+prescribers_raw <- prescribers_raw[nonstatematches == TRUE]
 mutate(prescribers_raw, total_rx = sum(ACETAMINOPHEN.CODEINE:ZOLPIDEM.TARTRATE))
 # Using gather to associate counts of prescriptions of individual drugs with providers.
 all_rx_per_npi <- gather(prescribers_raw, rx, countrx, ABILIFY:ZOLPIDEM.TARTRATE)
@@ -48,18 +40,10 @@ all_rx_op <- all_rx_per_npi[all_rx_per_npi$Opioid.Prescriber == 1,]
 all_rx_non <- all_rx_per_npi[all_rx_per_npi$Opioid.Prescriber == 0,]
 sum(all_rx_op$countrx)
 summary(all_rx_per_npi)
-# 10 drugs are identified as opioids from opioids.csv. Non-matching labels require manual 
+# 11 drugs are identified as opioids from opioids.csv. Non-matching labels require manual 
 # construction of a matching vector
 
 # N.B. All drugs identified as present in 2014 dataset are also in 2016 dataset
-
-matches <- c("ACETAMINOPHEN.CODEINE", "FENTANYL", "HYDROCODONE.ACETAMINOPHEN", "HYDROMORPHONE.HCL", "METHADONE.HCL",
-             "MORPHINE.SULFATE", "MORPHINE.SULFATE.ER", "OXYCODONE.HCL", "OXYCODONE.ACETAMINOPHEN",
-             "OXYCONTIN", "TRAMADOL.HCL")
-
-# Carisoprodol is omitted from analysis of opioids as single-agent formulation is not present in opioid listing.
-matching_records <- all_rx_per_npi$rx %in% matches
-matching_records_16 <- all_rx_per_npi_16$rx %in% matches
 
 # Missing opioids from prescriber data:
 # Buprenorphine, Buprenorphine HCl, BUTALBIT/ACETAMIN/CAFF/CODEINE, BUTORPHANOL TARTRATE,
@@ -71,9 +55,18 @@ matching_records_16 <- all_rx_per_npi_16$rx %in% matches
 # PENTAZOCINE HCL/NALOXONE HCL, PENTAZOCINE LACTATE, TAPENTADOL HCL, TRAMADOL HCL/ACETAMINOPHEN
 
 
-sum(prescribers_raw$Opioid.Prescriber)
 
-# 14,688 prescribers have value of Opioid Prescriber = 1, assumed to be True
+matches <- c("ACETAMINOPHEN.CODEINE", "FENTANYL", "HYDROCODONE.ACETAMINOPHEN", "HYDROMORPHONE.HCL", "METHADONE.HCL",
+             "MORPHINE.SULFATE", "MORPHINE.SULFATE.ER", "OXYCODONE.HCL", "OXYCODONE.ACETAMINOPHEN",
+             "OXYCONTIN", "TRAMADOL.HCL")
+
+# Carisoprodol is omitted from analysis of opioids as single-agent formulation is not present in opioid listing.
+matching_records <- all_rx_per_npi$rx %in% matches
+matching_records_16 <- all_rx_per_npi_16$rx %in% matches
+
+
+sum(prescribers_raw$Opioid.Prescriber)
+# 14503 prescribers have value of Opioid Prescriber = 1, assumed to be True
 
 # Slicing gathered dataset for only opioid drugs and Opioid prescribers. 
 opioid_per_npi <- all_rx_per_npi[matching_records == TRUE,]
@@ -86,10 +79,24 @@ View(count_of_opioids)
 count_of_opioids <- opioid_bynpi_hirx %>% 
   group_by(rx) %>% 
   summarise(total = sum(countrx))
+opioids_per_state <- 
+  opioid_bynpi_hirx %>% 
+  group_by(State) %>% 
+  filter(State %in% c("MT", "RI", "NH"))
 
-total_op_rx_state <- opioid_bynpi_hirx %>% 
-  group_by(as.factor(State)) %>% 
-  summarise(count(as.factor(State)))
+ggplot(opioids_per_state, aes(x = reorder(State, countrx), y = countrx)) +
+  geom_jitter() +
+#  labs(x = "Drug", y = "Total Prescriptions") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(count_of_opioids, aes(x = reorder(rx, total), y = total)) +
+       geom_col() +
+      labs(x = "Drug", y = "Total Prescriptions") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+avg_all_opioids <- mean(count_of_opioids$total)
+avg_all_opioids
+
 avgrx <- sum(all_rx_per_npi$countrx) / 25000
 avgrx
 sdrx <- sd(all_rx_per_npi$countrx)
@@ -102,14 +109,24 @@ opcount <- sum(opioid_bynpi_hirx$countrx) / 14503
 
 avgrx_non <- sum(all_rx_non$countrx) / 10497
 avgrx_non
-non_prescribers %>% 
+countperstate <- all_rx_non %>% 
   group_by(State) %>% 
-  summarise(sum(countrx))
+  summarise(total = sum(countrx))
+View(opioidprescriberperstate)
+mean(countperstate$total)
+ggplot(data = opioidprescriberperstate, aes(x = reorder(State, total), y = total)) +
+         geom_col()
 
-ggplot(data = count_of_opioids, aes(x = rx, y=total)) +
-         geom_bar()
+opioidprescriberperstate <- 
+  prescribers_raw %>% 
+  group_by(State) %>% 
+  summarise(total = sum(Opioid.Prescriber))
+View(opioidprescriberperstate)
 
+mutate(overdoses_raw, NormDeaths, as.numeric(Deaths)/as.numeric(Population))
 
-
-
+ggplot(overdoses_raw, aes(x = reorder(State, NormDeaths), y = NormDeaths)) +
+  geom_col() +
+  labs(x = "State", y = "Deaths Per Capita") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
